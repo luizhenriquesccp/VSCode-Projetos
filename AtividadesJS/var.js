e21385f0ef238e962f16b1fc04fdb3487c0bcb41
luizhenriquesccp <147549836+luizhenriquesccp@users.noreply.github.com>
2025-07-12 20:07:14 -0300

    atualizando o arquivo var.js, com um exemplo pratico para estudo.

--- AtividadesJS/var.js
+++ AtividadesJS/var.js
@@ -1,7 +1,9 @@
-var x = 7
-
 console.log('Variáveis (var) são globais, ou seja, podem ser acessadas fora do escopo onde foram definidas (em qualquer lugar do código).');
 console.log('Diferente de let que só funciona a onde ele foi declarado.'    + "' E const que não pode ser reatribuída.'");
+if (true) {
+    var x = 5;
+}
+
 console.log('O primeiro valor de x (variável var) é:', x);
 
 var x = 10;
